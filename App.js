06a9f8b76fc933a567a10858fb256aba9aa2941c
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-12-26 22:22:11 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,38 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMoviesState] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [errorState, setErrorState] = useState(null);
 
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
-    setError(null);
+    setErrorState(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const data = await response.json();
-
-      const transformedMovies = data.results.map((movieData) => {
+      const starWarsMoviesResponse = await fetch(
+        "https://swapi.dev/api/films/"
+      ).then((Response) => {
+        if (!Response.ok) {
+          throw new Error(Response.status);
+        }
+        return Response.json();
+      });
+      const moviesFormated = starWarsMoviesResponse.results.map((movie) => {
         return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
         };
       });
-      setMovies(transformedMovies);
+      setMoviesState(moviesFormated);
+      setIsLoading(false);
     } catch (error) {
-      setError(error.message);
+      setErrorState(error + ".  Something went wrong");
     }
     setIsLoading(false);
   }, []);
@@ -49,8 +51,8 @@ function App() {
     content = <MoviesList movies={movies} />;
   }
 
-  if (error) {
-    content = <p>{error}</p>;
+  if (errorState) {
+    content = <p>{errorState}</p>;
   }
 
   if (isLoading) {
